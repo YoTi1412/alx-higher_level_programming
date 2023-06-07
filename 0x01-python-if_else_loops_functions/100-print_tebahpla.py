dd532b59d9c36e961d801e30376e5ed73d6c7808
Yasser Rafai <yasserrafai5@gmail.com>
2023-06-06 18:17:02 -0700

    task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(i)) if not i % 2
-          else "{}".format(chr(i - 32)), end="")
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
