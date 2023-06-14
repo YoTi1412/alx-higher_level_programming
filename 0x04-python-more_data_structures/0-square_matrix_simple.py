f31e6c694ac2e4f109283ee7e28a20136f466860
YoTi <yasserrafai5@gmail.com>
2023-06-14 12:37:39 +0100

    add betty style

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    """
+    wordA function that computes the square
+    value of all integers of a matrix.
+    """
     new_matrix = []
     for col in matrix:
         result = list(map(lambda x: x**2, col))
